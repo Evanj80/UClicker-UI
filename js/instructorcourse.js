25f8400f68f5f5fbc38162c249a39e9896fc776f
architjain123 <46052746+architjain123@users.noreply.github.com>
2021-05-04 01:41:50 -0700

    added current class attendance and status

--- js/instructorcourse.js
+++ js/instructorcourse.js
@@ -13,21 +13,6 @@ courselist.forEach( (e) => {
     }
 });
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-type: 'pie',
-data: {
-    labels: ["Present", "Absent"],
-    datasets: [{
-    backgroundColor: [
-        "#2ecc71",
-        "#e74c3c"
-    ],
-    data: [12, 19]
-    }]
-}
-});
-
 function getAllStudents(){
     var xhr = new XMLHttpRequest();
     var url = urlmain + "get/all_students_class";
@@ -38,7 +23,13 @@ function getAllStudents(){
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let allstudents = JSON.parse(xhr.responseText);
+            let currentcount = 0;
+            let totalcount = 0
             allstudents.forEach( (student) => {
+                if (student.isAttending){
+                    currentcount+=1;
+                }
+                totalcount+=1;
                 let thiscourse = null;
                 student.classes.forEach( course => {
                     if (course.class_name === coursetitle){
@@ -52,6 +43,28 @@ function getAllStudents(){
                                         </div>`
                 statisticsdiv.innerHTML += studentcontainer
             });
+
+            if (isClassActive(currentcourse.days, currentcourse.start_time, currentcourse.end_time)){
+                var ctx = document.getElementById("myChart").getContext('2d');
+                var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ["Present", "Absent"],
+                    datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#e74c3c"
+                    ],
+                    data: [currentcount, totalcount-currentcount]
+                    }]
+                }
+                });
+                document.getElementById("classstatus").innerHTML = "Class in progress..."
+            }
+            else{
+                document.getElementById("classstatus").innerHTML = "Class not started"
+            }
+            
         }
         else if(xhr.status > 299 && xhr.readyState == 4) {
             let json = JSON.parse(xhr.responseText);
@@ -63,6 +76,34 @@ function getAllStudents(){
 }
 
 
+function isClassActive(classdaylist, classstart, classend){
+    let classdaylistnew = classdaylist.map(name => name.toLowerCase());
+    let today = new Date();  
+    let day = today.getDay();  
+    let hour = today.getHours();  
+    let min = today.getMinutes();
+    let shour = parseInt(classstart.substring(0, 2));
+    let smin = parseInt(classstart.substring(3, 5));
+    let ehour = parseInt(classend.substring(0, 2));
+    let emin = parseInt(classend.substring(3, 5));
+    let daylist = ["sunday","monday","tuesday","wednesday ","thursday","friday","saturday"];
+    let todayday = daylist[day];
+    if (classdaylistnew.includes(todayday)){
+        if (hour > shour && hour < ehour) {
+            return true;
+       } else if (hour == shour && min >= smin) {
+           return true;  
+       } else if (hour == ehour && min <= emin) {
+           return true;
+       } else {
+            return false;
+       }
+    }
+    return false;
+}
+
+
+console.log(isClassActive(["tuesday"], "01:00", "02:00"))
 
 
 document.getElementById("backarrow").addEventListener("click", (e) => {
