6595be96f165964b3821fb49c313146dea225d11
architjain123 <46052746+architjain123@users.noreply.github.com>
2021-05-04 01:03:47 -0700

    Added total student statistics

--- js/instructorcourse.js
+++ js/instructorcourse.js
@@ -1,6 +1,9 @@
+let urlmain = "http://uclickerapi-env-1.eba-gr7abipg.us-west-1.elasticbeanstalk.com/";
 let coursetitle = localStorage.getItem("current_course");
 let courselist = JSON.parse(localStorage.getItem("classes"));
-let currentcourse = null
+let currentcourse = null;
+
+getAllStudents();
 
 document.getElementById("coursetitle").innerHTML = coursetitle;
 
@@ -9,8 +12,6 @@ courselist.forEach( (e) => {
         currentcourse = e;
     }
 });
-
-console.log(currentcourse);
 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
@@ -26,6 +27,42 @@ data: {
     }]
 }
 });
+
+function getAllStudents(){
+    var xhr = new XMLHttpRequest();
+    var url = urlmain + "get/all_students_class";
+    let email = localStorage.getItem("email");
+    let statisticsdiv = document.getElementById("statistics");
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let allstudents = JSON.parse(xhr.responseText);
+            allstudents.forEach( (student) => {
+                let thiscourse = null;
+                student.classes.forEach( course => {
+                    if (course.class_name === coursetitle){
+                        thiscourse = course;
+                    }
+                });
+                attendence = "Attendence: " + thiscourse.attend + " / " + thiscourse.total_class_sessions;
+                let studentcontainer = `<div class="card" style="margin:5px;">
+                                            <h6 class="card-title" style="width: 400px;">${student.name}</h4>
+                                            <h6 class="text-muted card-subtitle mb-2" style="width: 400px;">${attendence}</h6>
+                                        </div>`
+                statisticsdiv.innerHTML += studentcontainer
+            });
+        }
+        else if(xhr.status > 299 && xhr.readyState == 4) {
+            let json = JSON.parse(xhr.responseText);
+            alert(json.error);
+        }
+    };
+    var data = JSON.stringify({"email": email, "class": coursetitle});
+    xhr.send(data);
+}
+
+
 
 
 document.getElementById("backarrow").addEventListener("click", (e) => {
